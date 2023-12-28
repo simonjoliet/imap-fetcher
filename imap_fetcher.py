623318c8a3335e61bc851c0add6227a32823d790
simonjoliet <simon.joliet@gmail.com>
2023-12-28 17:28:40 -0500

    Update imap_fetcher.py
    * Decoded Sender of UTF-9 &ISO-8859-1
    * Updated progress bar & status

--- imap_fetcher.py
+++ imap_fetcher.py
@@ -5,6 +5,9 @@ import io
 import sys
 import time
 
+def decode_subject(encoded_subject):
+    return ' '.join([part.decode(encoding) if encoding else part for part, encoding in email.header.decode_header(encoded_subject)])
+
 # Set the login credentials
 email_address = "your_email@gmail.com"
 password = "Google one time password"
@@ -48,14 +51,14 @@ for i, email_id in enumerate(email_ids):
     # Update the progress bar and remaining time
     progress = (i + 1) / num_emails
     filled = int(bar_width * progress)
-    bar = "[" + "\u2588" * filled + " " * (bar_width - filled) + "]"
+    bar = "" + "█" * filled + "░" * (bar_width - filled) + ""
     elapsed_time = time.time() - start_time
     if elapsed_time > 0:
         remaining_time = (elapsed_time / progress) - elapsed_time
     (hours, remainder) = divmod(int(remaining_time), 3600)
     (minutes, seconds) = divmod(remainder, 60)
     remaining_time_str = f"{hours:02}:{minutes:02}:{seconds:02}" if remaining_time else "--:--:--"
-    sys.stdout.write(f"\rProcessing emails: {bar} {progress:.1%} [{i+1} / {num_emails}] (remaining time: {remaining_time_str})")
+    print(f"\r{bar} {progress:.1%} [{i+1}/{num_emails}] {remaining_time_str} remaining", end="")
     sys.stdout.flush()
 
 # Close the connection to the IMAP server
@@ -74,6 +77,15 @@ writer.writerow(["Sender", "Email Count", "Total Size (MB)"])
 for sender, data in senders:
     email_count = data["count"]
     total_size_mb = round(data["size"] / (1024 * 1024), 2)
+
+    #Decode sender
+    parts = sender.split(' ')
+    if len(parts) > 1 and parts[0].startswith("=?") and parts[0].endswith("?="):
+        encoded_subject = ' '.join(parts[:-1])
+        decoded_subject = decode_subject(encoded_subject)
+        email_address = parts[-1]
+        sender = f"{decoded_subject} {email_address}"
+    
     writer.writerow([sender, email_count, total_size_mb])
 
 # Save the CSV file to disk
